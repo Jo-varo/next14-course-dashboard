d57d9ebe6f65cc24256bd1ff7582b93391fed412
jovaro <jorgevargas0240@gmail.com>
2024-01-30 20:34:13 -0500

    End of the course

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Montserrat, Inter, Lusitana } from 'next/font/google';
+
+export const montserrat = Montserrat({ subsets: ['latin'] });
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+});
